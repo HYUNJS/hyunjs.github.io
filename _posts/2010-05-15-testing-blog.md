274e8dde67089a694d824262e900e6a13b4f601f
HYUN Jeongseok <jhyunaa@connect.ust.hk>
2020-04-02 03:02:25 +0900

    [blog] bi-linear cnn for FGVR task

--- _posts/2010-05-15-testing-blog.md
+++
@@ -1,8 +0,0 @@
----
-title: "Testing"
-categories:
-  - Testing
-tags:
-  - testing
----
-## hi
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,8 +4,8 @@ var store = [{
         "tags": ["content","css","edge case","lists","markup"],
         "url": "http://localhost:4000/edge%20case/edge-case-nested-and-mixed-lists/",
         "teaser":"http://localhost:4000/"},{
-        "title": "Testing",
-        "excerpt":"hi  ","categories": ["Testing"],
-        "tags": ["testing"],
-        "url": "http://localhost:4000/testing/testing-blog/",
+        "title": "Bi-linear CNN models for fine-grained visual recognition",
+        "excerpt":"Key point Bilinear CNN utilizes the orderless descriptors which are more powerful to solve the texture classification than the orderful descriptors which store the additional spatial information. Summary Generally, CNN architecture for image classification is composed of convolutional layer and fully connected (FC) layer. At first, conv layer extracts the...","categories": ["Computer Vision"],
+        "tags": ["Fine-grained Visual recognition","CNN arch"],
+        "url": "http://localhost:4000/computer%20vision/bilinear-CNN-Models-for-Fine-grained-Visual-Recognition/",
         "teaser":"http://localhost:4000/"}]
